83d4ee11a79068074378f9fd9518962be1496db4
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-20 21:32:30 +0200

    Added db-config.ts and export configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TYPEORM_CONFIG } from './config/db-config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forRoot(TYPEORM_CONFIG)],
   controllers: [AppController],
   providers: [AppService],
 })
