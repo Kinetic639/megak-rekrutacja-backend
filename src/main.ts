96b1cf9a516a2f8a1520c5974fd851b143151f16
hubertkucharski <hubertkucharski@gmail.com>
2022-07-20 20:36:30 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3000',
+  });
   app.use(cookieParser());
   await app.listen(3001);
 }
