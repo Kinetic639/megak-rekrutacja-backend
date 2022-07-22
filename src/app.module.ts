db7ecdfb2d2724d8b606d0ef915d3dc7fefdb278
hubertkucharski <hubertkucharski@gmail.com>
2022-07-22 20:34:19 +0200

    added admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AuthModule } from './auth/auth.module';
 import { PanelModule } from './panel/panel.module';
 import { TYPEORM_CONFIG } from './config/db.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TYPEORM_CONFIG), AuthModule, PanelModule],
+  imports: [
+    TypeOrmModule.forRoot(TYPEORM_CONFIG),
+    AuthModule,
+    PanelModule,
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
