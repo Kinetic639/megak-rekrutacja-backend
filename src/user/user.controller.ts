355fb6c0df55ef96b2cefca2abee28dd0ddfc88a
Kinetic639 <michal.stepien36@gmail.com>
2022-07-29 18:18:47 +0200

    Fixed validation of students. Existing email now updates data of student instead of ignoring.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,9 +10,10 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
-import { CreateNewHr } from '../types/hr/create-new-hr';
+import { CreateNewHr } from '../types';
 import { FileInterceptor } from '@nestjs/platform-express';
 import * as CSV from 'csv-string';
+import { createUsersResponse } from '../types';
 
 @Controller('user')
 export class UserController {
@@ -27,9 +28,13 @@ export class UserController {
   @Post('/create/students')
   @UseGuards(AuthGuard('admin'))
   @UseInterceptors(FileInterceptor('file_asset'))
-  async createStudents(@UploadedFile() file: any) {
-    const parsedCsv = CSV.parse(file.buffer.toString(), { output: 'objects' });
-    console.log(parsedCsv);
+  async createStudents(
+    @UploadedFile() file: any,
+  ): Promise<createUsersResponse> {
+    const parsedCsv = CSV.parse(file.buffer.toString(), {
+      output: 'objects',
+    });
+    return this.userService.createdStudents(parsedCsv);
   }
 
   @Post('/create/hr')
