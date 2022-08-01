8aa3034284d69462d377bf0c6ccff1078d3b1517
Artur Masłowski <bolo0010.pl@gmail.com>
2022-08-01 15:41:17 +0200

    Update typeOrm.config.example.ts

--- src/config/typeOrm.config.example.ts
+++ src/config/typeOrm.config.example.ts
@@ -1,4 +1,4 @@
-/ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 //
 // export const TYPEORM_CONFIG: TypeOrmModuleOptions = {
 //   type: 'mysql',
