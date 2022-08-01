6457bceed4ee8c30b0d0150cf6978f050b7730d5
hubertkucharski <hubertkucharski@gmail.com>
2022-08-01 23:36:21 +0200

    added filterStudents functionality

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { COOKIE_SECRET } from './config/secrets';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser(COOKIE_SECRET));
   await app.listen(3001);
 }
