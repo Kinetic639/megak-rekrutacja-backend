7248796105b8db3b0fa9c59be0f4f7e21ec58a45
hubertkucharski <hubertkucharski@gmail.com>
2022-08-01 23:41:56 +0200

    added StudentModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TYPEORM_CONFIG } from './config/typeOrm.config';
 import { AdminModule } from './admin/admin.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailModule } from './mail/mail.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { MailModule } from './mail/mail.module';
     PanelModule,
     AdminModule,
     MailModule,
+    StudentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
