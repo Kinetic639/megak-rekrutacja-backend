7e161fdeee3272fc76e742452378f0e2014d9e9e
hubertkucharski <hubertkucharski@gmail.com>
2022-08-01 23:36:21 +0200

    added filterStudents functionality

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { FilterDto } from './dto/filter-student.dto';
+import { User } from '../user/user.entity';
+import { ContractTypeMap, ScoreMap, WorkTypeMap } from '../types';
+import { Between, In, IsNull, MoreThanOrEqual, Not } from 'typeorm';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  async findByFilter(query: FilterDto) {
+    const {
+      expectedContractType,
+      expectedTypeWork,
+      expectedSalaryFrom,
+      expectedSalaryTo,
+      courseCompletion,
+      courseEngagement,
+      monthsOfCommercialExp,
+      teamProjectDegree,
+      projectDegree,
+      canTakeApprenticeship,
+    } = query;
+
+    //Can't finde better solution to find every elements if query is null
+    //have to create arrays of enums
+    //if you got better solution I'm open for suggestions
+    console.log(
+      expectedContractType,
+      expectedTypeWork,
+      expectedSalaryFrom,
+      expectedSalaryTo,
+      courseCompletion,
+      courseEngagement,
+      monthsOfCommercialExp,
+      teamProjectDegree,
+      projectDegree,
+      canTakeApprenticeship,
+    );
+    return await User.find({
+      where: {
+        expectedContractType: In([expectedContractType ?? ContractTypeMap]),
+        expectedTypeWork: In([expectedTypeWork ?? WorkTypeMap]),
+        //if expectedSalary in db is null will not show (could be use to omit admin & hr filter)
+        expectedSalary: Between(expectedSalaryFrom, expectedSalaryTo),
+        courseCompletion: In(courseCompletion ?? ScoreMap),
+        courseEngagement: In(courseEngagement ?? ScoreMap),
+        monthsOfCommercialExp: MoreThanOrEqual(monthsOfCommercialExp ?? 0),
+        teamProjectDegree: In(teamProjectDegree ?? ScoreMap),
+        projectDegree: MoreThanOrEqual(projectDegree ?? 0),
+        canTakeApprenticeship: canTakeApprenticeship ?? Not(IsNull()),
+      },
+    });
+  }
+}
