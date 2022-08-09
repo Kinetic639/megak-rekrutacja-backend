940a637bcfb8761bcdd8c5bb2cb00560ae7bbdf4
hubertkucharski <hubertkucharski@gmail.com>
2022-08-09 16:57:37 +0200

    fix duplicated names

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,6 @@ import { MailModule } from './mail/mail.module';
 import { StudentModule } from './student/student.module';
 import { HrModule } from './hr/hr.module';
 import { UserModule } from './user/user.module';
-import { StudentModule } from './student/student.module';
-import { HrModule } from './hr/hr.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
